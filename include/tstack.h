18886f5a14a52732b0e05ea0398127ff9822fa20
larshinsr <82053310+larshinsr@users.noreply.github.com>
2023-04-02 01:47:30 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,22 @@
-// Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
-template<typename T, int size>
+template <typename T, int size>
 class TStack {
-  // добавьте код стека
-};
+ private:
+     int top;
+     T arr[size] = {0};
 
+ public:
+     TStack() : top(-1) {}
+     bool isFull() const { return size == top - 1; }
+     bool isEmpty() { return -1 == top; }
+     void push(T value) {
+         if (!isFull()) {
+             arr[++top] = value;
+         }
+     }
+     T get() const { return arr[top]; }
+     T pop() { return arr[top--]; }
+};
 #endif  // INCLUDE_TSTACK_H_
